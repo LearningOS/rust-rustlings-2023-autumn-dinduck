71c6fb1ec19958bdcb0c8f8a3f451ca0b2d37784
dingduck <kfove.liang@gmail.com>
2023-10-09 11:41:14 +0800

    vx Done

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -3,10 +3,11 @@
 // Execute `rustlings hint variables2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let x;
+    // 若只是声明变量 let x, rust会推导变量类型， 但是此时rust并不知道x的类型, let x: u32;
+    // 这样rust能知道x是u32类型， 但是此时是 uninit, let x = 0; , let x = 0u; let x = 0i;
+    // 这样rust可以推导x的类型
+    let x = 0;
     if x == 10 {
         println!("x is ten!");
     } else {
--- exercises/variables/variables4.rs
+++ exercises/variables/variables4.rs
@@ -3,10 +3,9 @@
 // Execute `rustlings hint variables4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let x = 3;
+    // Rust 的变量默认是不可变的, let mut 代表可变变量, 与const不同, let仍属于局部变量
+    let mut x = 3;
     println!("Number {}", x);
     x = 5; // don't change this line
     println!("Number {}", x);
