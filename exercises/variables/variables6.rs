c2757a5b8e69de83f84cab0b0c25ae5a4a7338e1
dingduck <kfove.liang@gmail.com>
2023-10-09 11:46:07 +0800

    v6 Done

--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -3,9 +3,10 @@
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
-const NUMBER = 3;
+// 全局的变量不能自动推断类型, let 实际是 Rust 的语法糖, 这里必须标注变量类型, static 和 const
+// 由于声明全局变量
+// 其中 *const 表示 &T , *mut &mut T, 这是 Rust 的地址指针
+const NUMBER: i32 = 3;
 fn main() {
     println!("Number {}", NUMBER);
 }
