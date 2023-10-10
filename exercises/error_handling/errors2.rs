fb4289784efbe3b21428b1101035533b70b8934d
dingduck <kfove.liang@gmail.com>
2023-10-10 23:13:03 +0800

    Done error

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -19,14 +19,13 @@
 // Execute `rustlings hint errors2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
+    // parse::<T>  可以转换 T, 也可 let x: T, 这样也可自动推导, 可以unwarp, 这里使用语法糖 ?
+    let qty = item_quantity.parse::<i32>()?;
 
     Ok(qty * cost_per_item + processing_fee)
 }
